cf3300e6b168b3b232e637187f0de02c49f410d4
samanthamarberger <slatham04@gmail.com>
2022-11-18 13:02:43 -0700

    Edited NavBar hrefs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,8 @@ function NavBar() {
   // update the JSX being returned!
   return (
   <nav>
-    <a href="home">Home</a>
-    <a href="about">About</a>
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
   </nav>
   );
 }
